710522f398c9f504d7e04b602fcdea602a7021d9
Tharumalingam Kantheepan <39334699+kanthee23@users.noreply.github.com>
2022-06-01 10:08:52 +0530

    Create Pythonquiz-2.py
    '''
    Python quiz - 2 Print the string in given Alignment !
    Twinkle, twinkle, little star,
    	How I wonder what you are!
    		Up above the world so high,
    		Like a diamond in the sky.
    Twinkle, twinkle, little star,
    	How I wonder what you are
    
    '''

---
+++ Pythonquiz-2.py
@@ -0,0 +1,21 @@
+'''
+Python quiz - 2
+
+Twinkle, twinkle, little star,
+	How I wonder what you are!
+		Up above the world so high,
+		Like a diamond in the sky.
+Twinkle, twinkle, little star,
+	How I wonder what you are
+
+'''
+
+def q2():
+
+    s = "Twinkle, twinkle, little star, \n\tHow I wonder what you are! \n\t\tUp above the world so high," \
+        " \n\t\tLike a diamond in the sky. \nTwinkle, twinkle, little star, \n\tHow I wonder what you are"
+
+    print(s)
+
+
+q2()
